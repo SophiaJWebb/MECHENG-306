d6b1253810c4b05b3a9f39bcab64e075d93b7445
Daniel Ryu <43049134+hamth@users.noreply.github.com>
2025-08-04 12:56:30 +1200

    Update encoderproto.ino

--- encoderproto.ino
+++ encoderproto.ino
@@ -4,44 +4,48 @@ float GEARRATIO = 171.79;
 int E1 = 5;
 int M1 = 4;
 
-int HALL1 = 20;
-int HALL2 = 21;
-int long count = 0;
-int hallOneLit = 0;
-int hallTwoLit = 0;
-int positive=true;
-bool direction=HIGH;
+int M1HALL1 = 2;
+int M2HALL1 = 3;
+// int M1HALL2 = 10; TBC
+// int M2HALL2 = 11; TBC
+int long m1count = 0;
+int long m2count = 0;
+
+int m1positive=true;
+bool m1direction=HIGH;
+
+int m2positive=true;
+bool m2direction=HIGH;
+
 const int CCW  = HIGH;
 const int CW = LOW;
-float position;
+
+float m1position;
+float m2position;
 
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(M1, OUTPUT);
-  pinMode(HALL1, INPUT);
-  pinMode(HALL2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(HALL1), HALLONE, RISING);
+  pinMode(M1HALL1, INPUT);
+  pinMode(M2HALL1, INPUT);
+  attachInterrupt(digitalPinToInterrupt(M1HALL1), M1HALLONE, RISING);
 }
 void loop()
 {
-  for (int value = 0; value <= 255; value++) {
-  if (value<128){
-    direction=CCW;
-  } else{
-    direction=CW;
-  }
-  digitalWrite(M1,direction);
+  value= 255;
+  m1direction=CW;
+  digitalWrite(M1,m1direction);
   analogWrite(E1, 255); //PWM Speed Control
-  position=count/GEARRATIO;
+  m1position=m1count/GEARRATIO;
 
   #ifdef DEBUG
   Serial.print("Count: ");
   Serial.print(position);
   Serial.print(" ");
   Serial.print("Direction: ");
-  Serial.println(positive);
+  Serial.println(m1positive);
   delay(10);  
   #endif // Debug
   }
@@ -49,20 +53,38 @@ void loop()
 
 
 
-void counting(){
-  if (positive) {
-    count++;
+void m1counting(){
+  if (m1positive) {
+    m1count++;
   } else{
-    count--;
+    m1count--;
   }
 }
 
-void HALLONE(){
-    if (digitalRead(HALL2)) {
-      positive=true;
+void m2counting(){
+  if (m2positive) {
+    m2count++;
+  } else{
+    m2count--;
+  }
+}
+
+void M1HALLONE(){
+    if (digitalRead(M1HALL2)) {
+      m1positive=true;
 
   } else {
-      positive=false;
+      m1positive=false;
   }
-  counting();
+  m1counting();
+}
+
+void M2HALLONE(){
+    if (digitalRead(M2HALL2)) {
+      m2positive=true;
+
+  } else {
+      m2positive=false;
+  }
+  m1counting();
 }
