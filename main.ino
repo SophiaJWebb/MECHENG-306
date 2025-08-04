101ef0448859ac4b1bfede4e7c2a76e097794160
Sophia Webb <sweb390@aucklanduni.ac.nz>
2025-08-04 04:27:29 +0000

    basic functionality for switching to homing and moving and then back to with Gcode commands

--- main.ino
+++ main.ino
@@ -1,37 +1,68 @@
-// State definitions
-enum State {
-  IDLE,
-  HOMING,
-  CALIBRATION,
-  MOVING,
-  ERROR
-};
+#include "GCode.hpp"
+//#include encoder 
+//#include
+#include <iostream>
+using namespace std;
 
-State currentState = IDLE;
+#define IDLE 0
+#define HOMING 1
+#define MOVING 2
+#define ERROR 3
+#define CALIBRATION 4
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
+
 }
 
-void loop() {
-  // Read buttons (active LOW)
+int main() {
+    // Read buttons (active LOW)
+    GCodeParser Parser;
+    bool running = true;
+    int currentState = IDLE;
+  
+    float absolute_x = -1;
+    float absolute_y = -1;
+    float change_in_x = 0;
+    float change_in_y = 0;
+    string command;
+    const float* parameters = nullptr;
 
-  switch (currentState) {
-    case IDLE:
+    while (running) {
+        switch (currentState) {
+            case IDLE: {
+                std::getline(std::cin, command);
+                int State = Parser.ExecuteCommand(command);
+                if (State == 1) {
+                    currentState = HOMING;
+                } else if (State == 2) {
+                    currentState = MOVING;
+                }
+                break;
+            }
+            case HOMING: {
+                std::cout << "Homing in progress..." << std::endl;
+                currentState = IDLE; // Reset to IDLE after homing 
+                break;
+            }
 
-      break;
-
-    case HOMING:
-
-      break;
-    case CALIBRATION:
-
-      break;
-    case MOVING:
-
-      break;  
-    case ERROR:
-
-      break;                
-  }
+            case MOVING: {
+                parameters = Parser.GetParameters();
+                change_in_x = parameters[0] - absolute_x;
+                change_in_y = parameters[1] - absolute_y;
+                cout << "Moving: " << endl;
+                // run move function 
+                currentState = IDLE; // Reset to IDLE after moving
+                break;
+            }  
+            case ERROR: {
+                std::cout << "Error occurred." << std::endl;
+                break;
+            }
+            case CALIBRATION: {
+                std::cout << "Calibration in progress..." << std::endl;
+                break;
+            }
+        }
+    }
 }
