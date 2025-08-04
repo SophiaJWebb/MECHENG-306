ac5d2db9ba0ef9207b0c987c30d539e4507c9088
Daniel Ryu <43049134+hamth@users.noreply.github.com>
2025-08-04 13:45:09 +1200

    SetUpForM1&M2.ino

--- encoderproto.ino
+++ encoderproto.ino
@@ -1,13 +1,17 @@
 //Arduino PWM Speed Control：
 #define DEBUG // Comment out to remove serial
-float GEARRATIO = 171.79;
+double GEARRATIO = 171.79;
+double COUNTTODISTANCERATIO = 65.618946 // For 1mm 65.61 counts are needed
 int E1 = 5;
 int M1 = 4;
+int E2 = 6;
+int M2 = 7;
 
 int M1HALL1 = 2;
 int M2HALL1 = 3;
 // int M1HALL2 = 10; TBC
 // int M2HALL2 = 11; TBC
+
 int long m1count = 0;
 int long m2count = 0;
 
@@ -20,15 +24,17 @@ bool m2direction=HIGH;
 const int CCW  = HIGH;
 const int CW = LOW;
 
-float m1position;
-float m2position;
+double m1position=0; //in mm
+double m2position=0; //in mm
 
 
 void setup()
 {
   Serial.begin(9600);
+
   pinMode(M1, OUTPUT);
-  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+
   pinMode(M1HALL2, INPUT);
   pinMode(M2HALL2, INPUT);
   attachInterrupt(digitalPinToInterrupt(M1HALL1), M1HALLONE, RISING);
@@ -39,24 +45,43 @@ void loop()
 {
   value= 255;
   m1direction=CW;
+  m2direction=CW;
   digitalWrite(M1,m1direction);
+  digitalWrite(M2,m2direction);
   analogWrite(E1, 255); //PWM Speed Control
-  m1position=m1count/GEARRATIO;
+  analogWrite(E1, 255); //PWM Speed Control
+  m1position=countToDistance(m1count);
+  m2position=countToDistance(m1count);
 
   #ifdef DEBUG
-  Serial.print("Count: ");
-  Serial.print(position);
+  Serial.print("M1Count: ");
+  Serial.print(m1position);
   Serial.print(" ");
-  Serial.print("Direction: ");
+  Serial.print("M1Direction: ");
   Serial.println(m1positive);
+
+  Serial.print("M2Count: ");
+  Serial.print(m2position);
+  Serial.print(" ");
+  Serial.print("M2Direction: ");
+  Serial.println(m2positive);
   delay(10);  
   #endif // Debug
   }
 }
 
+double countToDistance(int count)
+{
+return (count/COUNTTODISTANCERATIO)
+}
 
+double distanceToCount(int distance)
+{
+return (distance*COUNTTODISTANCERATIO)
+}
 
-void m1counting(){
+void m1counting()
+{
   if (m1positive) {
     m1count++;
   } else{
@@ -64,7 +89,8 @@ void m1counting(){
   }
 }
 
-void m2counting(){
+void m2counting()
+{
   if (m2positive) {
     m2count++;
   } else{
@@ -72,7 +98,8 @@ void m2counting(){
   }
 }
 
-void M1HALLONE(){
+void M1HALLONE()
+{
     if (digitalRead(M1HALL2)) {
       m1positive=true;
 
@@ -82,7 +109,8 @@ void M1HALLONE(){
   m1counting();
 }
 
-void M2HALLONE(){
+void M2HALLONE()
+{
     if (digitalRead(M2HALL2)) {
       m2positive=true;
 
