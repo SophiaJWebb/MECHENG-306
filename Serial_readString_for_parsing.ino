472ba1dcc1f0d3ec4bb19ca03c5810e2e99ef9ff
Sophia Webb <sweb390@aucklanduni.ac.nz>
2025-08-04 13:25:56 +1200

    Add files via upload

---
+++ Serial_readString_for_parsing.ino
@@ -0,0 +1,17 @@
+String incomingByte; // for incoming serial data
+
+void setup() {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  // send data only when you receive data:
+  while (Serial.available()) {
+    // read the incoming byte:
+    incomingByte = Serial.readString();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte);
+  }
+}
