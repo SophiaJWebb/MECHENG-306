08bb033d47fbbaad6b8aac61ff4496c566e8e0e0
bruh <rlin108@aucklanduni.ac.nz>
2025-08-15 16:20:20 +1200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,3 +1,4 @@
+#include "GCode.hpp"
 #include "definitions.hpp"
 
 int long left_last_time = 0;
@@ -21,9 +22,12 @@ bool right_limit_calibration = false;
 bool top_limit_calibration = false;
 bool bottom_limit_calibration = false;
 
+GCodeParser Parser;
+
 using namespace std;
 
-void setup() {
+void setup() 
+{
   pinMode(LEFT_INTERRUPT_PIN, INPUT);
   pinMode(RIGHT_INTERRUPT_PIN, INPUT);
   pinMode(TOP_INTERRUPT_PIN, INPUT);
@@ -38,37 +42,79 @@ void setup() {
 }
 
 void loop() {
-  while (1) {
+    while (1) {
     switch (STATE) {
       case IDLE: {
         Serial.println("State Idle");
         Serial.println("Enter GCode command");
         while (Serial.available() == 0){
         }
-        command = Serial.readStringUntil('\n');  // Read until newline
-        int State = Parser.ExecuteCommand(command.c_str());
-        if (State == 1) {
-          currentState = HOMING;
-        } else if (State == 2) {
-          currentState = MOVING;
-        }
+        String command = Serial.readStringUntil('\n');  // Read until newline
+        STATE = Parser.ExecuteCommand(command.c_str());
         break;
-    }
       }
       case HOMING: {
         // Run homing routine
+        Serial.println("Running homing routine");
+       //Move down
+    digitalWrite(M1, CW);
+    digitalWrite(M2, CCW);
+    analogWrite(E1, 100);  // Can increase speed later
+    analogWrite(E2, 100);
+    // move until bottom switch is pressed
+    while (!bottom_hit){
+      Serial.println(bottom_hit);
+    }
+    Serial.println("exited while loop");
+    bottom_hit = false;
+    digitalWrite(M1, CW);
+    digitalWrite(M2, CCW);
+    analogWrite(E1, 255);  
+    analogWrite(E2, 255);
+    delay(1000); // change from delay 
+    //back up until encoder count is tbd 
+    digitalWrite(M1, CCW);
+    digitalWrite(M2, CW);
+    analogWrite(E1, 100);  
+    analogWrite(E2, 100);
+    while (!bottom_hit){
+    }
+    bottom_hit = false;
+    // Set absolute_x 0
+
+    // //move left
+    // digitalWrite(M1, CCW);
+    // digitalWrite(M2, CCW);
+    // analogWrite(E1, 100);  // Can increase speed later
+    // analogWrite(E2, 100);
+    // // move until left switch is pressed
+    // while (!left_hit){
+    // }
+    // left_hit = false;
+    // digitalWrite(M1, CCW);
+    // digitalWrite(M2, CCW);
+    // analogWrite(E1, 70);  
+    // analogWrite(E2, 70);
+    // delay(1000); // change from delay 
+    // //back up until encoder count is tbd 
+    // digitalWrite(M1, CCW);
+    // digitalWrite(M2, CCW);
+    // analogWrite(E1, 70);  
+    // analogWrite(E2, 70);
+    // while (!left_hit){
+    // }
+    // bottom_hit = false;
+
+        STATE = IDLE;
         break;
       }
       case MOVING: {
-        // Move bracket
+        Serial.println("Running moving routine");
+        STATE = IDLE;
         break;
       }
       case ERROR: {
         // Fix error then rehome
-        break;
-      }
-      case CALIBRATION: {
-        // Calibrate
         break;
       }
     }
@@ -78,12 +124,16 @@ void loop() {
 void left_limit_switch_hit() {
   left_now = millis();
   if (left_now - left_last_time > DEBOUNCE_DELAY_MS) {
-    if (STATE != CALIBRATION) {
+    if (STATE != HOMING) {
       error_running = true; // Flag to change state to ERROR
     } else {
       left_limit_calibration = true; // Flag to tell when switch has been hit while in calibration
     }
     Serial.println("Left limit switch hit");
+    left_hit = true;
+    // Stop motors
+    analogWrite(E1, 0);
+    analogWrite(E2, 0);
   }
   left_last_time = left_now;
 }
@@ -91,38 +141,62 @@ void left_limit_switch_hit() {
 void right_limit_switch_hit() {
   right_now = millis();
   if (right_now - right_last_time > DEBOUNCE_DELAY_MS) {
-    if (STATE != CALIBRATION) {
+    if (STATE != HOMING) {
       error_running = true; // Flag to change state to ERROR
     } else {
       right_limit_calibration = true; // Flag to tell when switch has been hit while in calibration
     }
     Serial.println("Right limit switch hit");
+    right_hit = true;
+
+    // Stop motors
+    analogWrite(E1, 0);
+    analogWrite(E2, 0);
   }
   right_last_time = right_now;
+
+  right_hit = true;
+
+  // Stop motors
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
 }
 
 void top_limit_switch_hit() {
   top_now = millis();
   if(top_now - top_last_time > DEBOUNCE_DELAY_MS) {
-    if (STATE != CALIBRATION) {
+    if (STATE != HOMING) {
       error_running = true; // Flag to change state to ERROR
     } else {
       top_limit_calibration = true; // Flag to tell when switch has been hit while in calibration
     }
     Serial.println("Top limit switch hit");
+    top_hit = true;
+
+    // Stop motors
+    analogWrite(E1, 0);
+    analogWrite(E2, 0);
+
   }
   top_last_time = top_now;
+
+  top_hit = true;
 }
 
 void bottom_limit_switch_hit() {
   bottom_now = millis();
   if (bottom_now - bottom_last_time > DEBOUNCE_DELAY_MS) {
-    if (STATE != CALIBRATION) {
+    if (STATE != HOMING) {
       error_running = true; // Flag to change state to ERROR
     } else {
       bottom_limit_calibration = true; // Flag to tell when switch has been hit while in calibration
     }
     Serial.println("Bottom limit switch hit");
+    //Serial.println(bottom_hit);
+    bottom_hit = true;
+    // Stop motors
+    analogWrite(E1, 0);
+    analogWrite(E2, 0);
   }
   bottom_last_time = bottom_now;
 }
