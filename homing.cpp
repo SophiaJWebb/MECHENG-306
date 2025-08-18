55692e058eab507579018325eb00e9307729cb4f
Sophia Webb <sweb390@aucklanduni.ac.nz>
2025-08-18 03:54:54 +0000

    homing routine

--- homing.cpp
+++ homing.cpp
@@ -13,50 +13,49 @@ const int CCW  = HIGH;
 const int CW = LOW;
 
 Homing() {
-    //Move down
-    DigitalWrite(M1, CCW);
-    DigitalWrite(M2, CW);
-    AnalogueWrite(E1, 100);  // Can increase speed later
-    AnalogueWrite(E2, 100);
-    // move until bottom switch is pressed
-    while (!bottom_hit){
-    }
-    bottom_hit = false;
-    DigitalWrite(M1, CW);
-    DigitalWrite(M2, CCW);
-    AnalogueWrite(E1, 70);  
-    AnalogueWrite(E2, 70);
-    delay(1000); // change from delay 
-    //back up until encoder count is tbd 
-    DigitalWrite(M1, CCW);
-    DigitalWrite(M2, CW);
-    AnalogueWrite(E1, 70);  
-    AnalogueWrite(E2, 70);
-    while (!bottom_hit){
-    }
-    bottom_hit = false;
-    // Set absolute_x 0
-
-    //move left
-    DigitalWrite(M1, CCW);
-    DigitalWrite(M2, CCW);
-    AnalogueWrite(E1, 100);  // Can increase speed later
-    AnalogueWrite(E2, 100);
-    // move until left switch is pressed
-    while (!left_hit){
-    }
-    left_hit = false;
-    DigitalWrite(M1, CCW);
-    DigitalWrite(M2, CCW);
-    AnalogueWrite(E1, 70);  
-    AnalogueWrite(E2, 70);
-    delay(1000); // change from delay 
-    //back up until encoder count is tbd 
-    DigitalWrite(M1, CCW);
-    DigitalWrite(M2, CCW);
-    AnalogueWrite(E1, 70);  
-    AnalogueWrite(E2, 70);
-    while (!left_hit){
-    }
-    bottom_hit = false;
+    digitalWrite(M1,CCW);
+  digitalWrite(M2,CW);
+  analogWrite(E1, 200); //PWM Speed Control
+  analogWrite(E2, 200); //PWM Speed Control
+  while(!hit){
+    Serial.println("hit: ");
+    Serial.print(hit);
+  }
+  digitalWrite(M1,CW);
+  digitalWrite(M2,CCW);
+  analogWrite(E1, value); //PWM Speed Control
+  analogWrite(E2, value);
+  delay(2000);
+  hit = false;
+  digitalWrite(M1,CCW);
+  digitalWrite(M2,CW);
+  analogWrite(E1, value); //PWM Speed Control
+  analogWrite(E2, value); //PWM Speed Control
+  while(!hit){
+    Serial.println("moving");
+  }
+//left homing
+  hit = false;
+  digitalWrite(M1,CW);
+  digitalWrite(M2,CW);
+  analogWrite(E1, 200); //PWM Speed Control
+  analogWrite(E2, 200); //PWM Speed Control
+  while(!hit){
+    Serial.println("hit: ");
+    Serial.print(hit);
+  }
+  digitalWrite(M1,CCW);
+  digitalWrite(M2,CCW);
+  analogWrite(E1, value); //PWM Speed Control
+  analogWrite(E2, value);
+  delay(2000);
+  hit = false;
+  digitalWrite(M1,CW);
+  digitalWrite(M2,CW);
+  analogWrite(E1, value); //PWM Speed Control
+  analogWrite(E2, value); //PWM Speed Control
+  while(!hit){
+    Serial.println("moving");
+  }
+  hit = false;
 }
