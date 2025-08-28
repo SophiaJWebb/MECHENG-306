13e116a692e32951e65fd4d66eafc6a975714067
Sophia Webb <sweb390@aucklanduni.ac.nz>
2025-08-28 22:52:49 +1200

    change y limit

--- GCode.cpp
+++ GCode.cpp
@@ -88,7 +88,7 @@ void GCodeParser::extractParameters(String tokens[], int tokenCount){
 bool GCodeParser::ValidateParameters(float currentX, float currentY) {
     // validate x and y distances
     if ((parameters[0] + currentX) > 225 || 
-        (parameters[1] + currentY) > 150 || 
+        (parameters[1] + currentY) > 140 || 
         (parameters[1] + currentY) < 0 || 
         (parameters[0] + currentX) < 0) {
         Serial.println("Distance entered exceeds limits");
