1300b823f40722d71c38f6088d2093857b43f739
Daniel Ryu <43049134+hamth@users.noreply.github.com>
2025-08-28 16:10:50 +1200

    Add files via upload

---
+++ Encoder.cpp
@@ -0,0 +1,61 @@
+#include "Encoder.h"
+#include <Arduino.h>
+
+Encoder::Encoder()
+{
+    encoderTicks_ = 0;
+    millimeters_ = 0.0;
+    lastEncoderTicks_ = 0;
+}
+
+Encoder::~Encoder()
+{
+
+}
+
+void Encoder::resetEncoder()
+{
+    encoderTicks_ = 0;
+    millimeters_ = 0.0;
+    lastEncoderTicks_ = 0;
+}
+
+long Encoder::getLastEncoderTicks() const
+{
+    return lastEncoderTicks_;
+}
+
+void Encoder::setLastEncoderTicks(long ticks)
+{
+    lastEncoderTicks_ = ticks;
+}
+
+long Encoder::convertMillimetersToTicks(double millimeters)
+{
+    long convertedTicks = millimeters * millimetersToTicks_;
+    return convertedTicks;
+}
+
+double Encoder::convertTicksToMillimeters(long ticks)
+{
+    double convertedMillimeters = ticks / millimetersToTicks_;
+    return convertedMillimeters;
+}
+
+void Encoder::countTicks(int amount)
+{
+    encoderTicks_ += amount;
+    absoluteEncoderTicks_ += amount;
+    millimeters_ = convertTicksToMillimeters(encoderTicks_);
+}
+
+long Encoder::getEncoderTicks() const
+{
+    return encoderTicks_;
+}
+
+double Encoder::getMillimeters() const
+{
+    return millimeters_;
+}
+
